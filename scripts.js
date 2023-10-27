4e8f30ddc8da1eab9ee6d045b69445da40d1ff5c
UlisesMicaias <ulisesmicaias@gmail.com>
2023-10-27 10:13:46 -0300

    mi segundo commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+console.log("HOLA MUNDO");
+
+let nombre = "ulises";
+nombre = "micaias";
+console.log(nombre);
+// let miRequest = {
+//     method: "POST",
+//     body : {
+//         user: "ulises2005",
+//         pass: "12345",
+//     },
+//     protocol: "HTTP1",
+//     headers: "Información complementaria",
+//     url: "a qué dirección realizamos el pedido",
+// };
+
+// let miResponse = {
+//     statusCode: ["1xx", "2xx", "3xx", "4xx","5xx"],
+//     message: "todo okey",
+//     body: "dependiendo del tipo de respuesta, puede existir un body",
+//     protocol: "ej HTTP1",
+//     headers: "información complementaria",
+// };
